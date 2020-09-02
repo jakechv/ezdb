93eb84f321f1024c91baf3bc03ec73a96671dc7d
jakechv <jakechvatal@gmail.com>
2020-09-02 19:43:57 +0200

    Start simple cli application

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-const {exec} = require ('child_process');
+// const { exec } = require('child_process');
+
+const yargs = require('yargs');
+
+const options = yargs.usage('Usage: -n <name>').option('n', {
+  alias: 'name',
+  describe: 'Your name',
+  type: 'string',
+  demandOption: true,
+}).argv;
+
+console.log(`Hello ${options.name}!`);
