3c15fe3e9795f2bcd8e327afc78280e7f5b0bf3c
jakechv <jakechvatal@gmail.com>
2020-09-02 23:56:38 +0200

    Suppress eslint warning

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs');
 // const { exec } = require('child_process');
 
-const options = yargs.usage('Usage: -n <name>').option('n', {
-  alias: 'name',
-  describe: 'Your name',
-  type: 'string',
-  demandOption: true,
-}).argv;
+/* eslint no-unused-expressions: 1 */
+const DEFAULT_DB_USER = 'username';
+const DEFAULT_DB_PASS = 'password';
+const DEFAULT_DB_NAME = 'db';
+const DEFAULT_DB_TEST = `${DEFAULT_DB_NAME}_test`;
 
-console.log(`Hello ${options.name}!`);
+const yargs = require('yargs');
+
+yargs
+  .scriptName('ezdb')
+  .usage('Usage: $0 <cmd> [options]')
+
+  .command('start', 'start the database')
+  .command('stop', 'stop the database')
+  .command('restart', 'restart the database')
+  .command('init', 'initialize the database')
+  .command('reset', 'reset the database')
+
+  .option('u', {
+    alias: 'username',
+    describe: 'The username for the user of the database.',
+    default: DEFAULT_DB_USER,
+    type: 'string',
+    nargs: 1,
+  })
+
+  .option('p', {
+    alias: 'password',
+    describe: 'The password for the user of the database.',
+    default: DEFAULT_DB_PASS,
+    type: 'string',
+    nargs: 1,
+  })
+
+  .option('d', {
+    alias: 'database',
+    describe: 'The name of the database to be created.',
+    default: DEFAULT_DB_NAME,
+    type: 'string',
+    nargs: 1,
+  })
+
+  .option('t', {
+    alias: 'test-database',
+    describe: 'The name of the test database to be created.',
+    default: DEFAULT_DB_TEST,
+    type: 'string',
+    nargs: 1,
+  })
+  .help('help')
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .demandCommand()
+  .showHelpOnFail(true).argv;
